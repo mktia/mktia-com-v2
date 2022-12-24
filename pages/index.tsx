c114c1035414b06077563b9041ce32cdda38252e
mktia <19624453+mktia@users.noreply.github.com>
2022-12-24 22:13:48 +0900

    update contents

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,20 +4,21 @@ import Layout from "../components/layout"
 
 const Home: NextPage = () => {
   const code = `
-  while (isFailed) {
-    challenge();
+  while (asLongAsILive) {
+    hasResult = challenge();
+    
+    if (hasResult) {
+      continue();
+    } else {
+      getNewSkill();
+      goToNextStep();
+    }
   }`
   return (
     <Layout>
       <div className="my-48 text-xl">
         <pre>{code}</pre><pre className="blink">  _</pre>
       </div>
-      <p>
-        乏しい文章力を向上させるためにブログ記事を書いていたら，いつの間にかプログラムも書けるようになっていました．
-      </p>
-      <p className="mt-4">
-        今はパブリックブロックチェーンに興味があり，DApp開発とかたまにしてます．
-      </p>
     </Layout>
   )
 }
